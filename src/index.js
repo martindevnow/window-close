f08a95ede558ddac6b912d8c055fde2e084b0f0c
Benjamin Martin <bmartin@atbventures.com>
2023-10-31 12:44:11 -0400

    Simple

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Alberta from "./Alberta";
+import Consent from "./Consent";
+import Success from "./Success";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/consent",
+    element: <Consent />,
+  },
+  {
+    path: "/alberta",
+    element: <Alberta />,
+  },
+  {
+    path: "/success",
+    element: <Success />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
